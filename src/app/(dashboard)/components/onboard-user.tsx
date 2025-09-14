29241088c521179d97bcc75fef15082f10dcc990
Akshat Dubey <akshatdubey0808@gmail.com>
2025-09-15 01:37:05 +0530

    fix: build issues with linting

--- src/app/(dashboard)/components/onboard-user.tsx
+++ src/app/(dashboard)/components/onboard-user.tsx
@@ -5,8 +5,8 @@ export default function OnboardUser() {
     <div className="flex-1 border-x border-dashed p-8 flex flex-col gap-4">
       <h1 className="text-6xl tracking-tight">Hey there!</h1>
       <p className="text-muted-foreground">
-        Welcome to Zeit, your wishlist app. Let's get started by setting up your
-        monthly income and working days.
+        Welcome to Zeit, your wishlist app. Let&apos;s get started by setting up
+        your monthly income and working days.
       </p>
       <CalculationDetailsForm />
     </div>
